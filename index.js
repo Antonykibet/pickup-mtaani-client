10166d8eb1c2df573c616a7420420b12eae91837
Antonykibet <antonykibet059@gmail.com>
2024-01-11 22:38:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+async function getAgents(){
+    let response = await fetch('https://new.pickupmtaani.com/api/agents')
+    return await response.json()
+}
+
+//renders HTML
+async function agentsRender(div){
+    let agents = await getAgents()
+    agents.forEach((item)=>{
+        let agentDiv = document.createElement('div')
+        let label = document.createElement('label')
+        let selectElem = document.createElement('select')
+        label.textContent=`${item.location}`
+        item.agents.forEach((item)=>{
+            selectElem.innerHTML+=`<option value='${item}'>${item}</option>`
+        })
+        agentDiv.append(label,selectElem)
+        div.append(agentDiv)
+    })
+}
